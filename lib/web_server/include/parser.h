b0794722d010df3ee214538e905ca68627457d42
Piotr Miszta <miszta.piotr.pm@gmail.com>
2025-03-02 13:18:20 +0100

    [ADD] Implement parser request handling

--- lib/web_server/include/parser.h
+++ lib/web_server/include/parser.h
@@ -1,7 +1,10 @@
 #ifndef WEB_SERVER_PARSER_H_
 #define WEB_SERVER_PARSER_H_
 #include "err_codes.h"
+#include "str.h"
 
 web_server_err_t parser_boot(uint32_t no_parser_threads);
+web_server_err_t parser_add_request(str_t request);
+web_server_err_t parser_close(void);
 
 #endif
--- lib/web_server/include/web_server.h
+++ lib/web_server/include/web_server.h
@@ -7,5 +7,6 @@ typedef struct web_server_t web_server_t;
 web_server_err_t web_server_alloc(web_server_t** server);
 web_server_err_t web_server_free(web_server_t* server);
 web_server_err_t web_server_run(web_server_t* server);
+web_server_err_t web_server_close(web_server_t* server);
 
 #endif
